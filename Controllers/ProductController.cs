6510a93244b374142431d78f454832132bdf6c0f
Halitbektas <114929752+Halitbektas@users.noreply.github.com>
2026-06-04 00:46:16 +0300

    eksik olan getall endpointi tamamlandı

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -84,5 +84,12 @@ namespace LLM_Destekli_Ozetleme.Controllers
 
             return Ok(popularProducts);
         }
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] ProductQueryParameters queryParams)
+        {
+            var products = await _productService.GetProductsAsync(queryParams);
+            return Ok(products);
+        }
     }
 }
