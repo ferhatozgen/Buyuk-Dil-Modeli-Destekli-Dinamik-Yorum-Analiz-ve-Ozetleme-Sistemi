1b6870c6840826c55eeed1b496904200dcfb0808
Ferhat Özgen <ferhatozgen17@gmail.com>
2026-06-21 22:08:57 +0300

    feat: örnek link yemeksepeti çalıştırıldı

--- algorithms-fastapi/functions/test_bot.py
+++ algorithms-fastapi/functions/test_bot.py
@@ -1,5 +1,4 @@
 from functions.scraper import yemeksepeti_veri_cek
 print("Test basliyor...")
-# Test için örnek bir link ver
-sonuc = yemeksepeti_veri_cek("https://www.yemeksepeti.com/restaurant/ornek-id/ornek-isim", 1)
+sonuc = yemeksepeti_veri_cek("https://www.yemeksepeti.com/restaurant/rvjp/kuzey-kokorec", 1)
 print("Test bitti, sonuc:", sonuc)
