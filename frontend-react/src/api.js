e84a5ed29feac1c64b3e55f803df405c5a482265
Ferhat Özgen <114931798+ferhatozgen@users.noreply.github.com>
2026-06-21 18:18:22 +0300

    Update api.js

--- frontend-react/src/api.js
+++ frontend-react/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://bubbly-enthusiasm-production-7c3a.up.railway.app/',
+    baseURL: 'https://bubbly-enthusiasm-production-7c3a.up.railway.app/api',
     headers: {
         'Content-Type': 'application/json'
     }
