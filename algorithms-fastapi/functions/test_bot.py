b98da3ae6433dad5a015a1b9cf24022a00334c54
Ferhat Özgen <ferhatozgen17@gmail.com>
2026-06-22 20:15:24 +0300

    test kısmı denemleri tamamlandı.Merge hazır

--- algorithms-fastapi/functions/test_bot.py
+++
@@ -1,4 +0,0 @@
-from functions.scraper import yemeksepeti_veri_cek
-print("Test basliyor...")
-sonuc = yemeksepeti_veri_cek("https://www.yemeksepeti.com/restaurant/rvjp/kuzey-kokorec", 1)
-print("Test bitti, sonuc:", sonuc)
